4fbd2526ccfb800b680b909726c5a1a2bfc674fb
NaokiM03 <mattsun.phone@gmail.com>
2019-03-03 23:30:11 +0900

    分解 Ver.

--- app/chapter03/src/Main.elm
+++ app/chapter03/src/Main.elm
@@ -6,11 +6,18 @@ import Html.Attributes exposing (..)
 
 main : Html msg
 main =
-    div []
-        [ h1 [] [ text "Useful Links" ]
-        , ul []
-            [ li [] [ a [ href "https://elm-lang.org" ] [ text "Homepage" ] ]
-            , li [] [ a [ href "https://package.elm-lang.org" ] [ text "Packages" ] ]
-            , li [] [ a [ href "https://ellie-app.com" ] [ text "Playground" ] ]
-            ]
+    div [] [ header, content ]
+
+
+header : Html msg
+header =
+    h1 [] [ text "Useful Links" ]
+
+
+content : Html msg
+content =
+    ul []
+        [ li [] [ a [ href "https://elm-lang.org" ] [ text "Homepage" ] ]
+        , li [] [ a [ href "https://package.elm-lang.org" ] [ text "Packages" ] ]
+        , li [] [ a [ href "https://ellie-app.com" ] [ text "Playground" ] ]
         ]
